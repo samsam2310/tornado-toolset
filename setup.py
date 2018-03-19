2f5d32b0672deacf8a75ebf4c4c10fe8f05a4cfb
Chung-Sheng Wu <samsam2310@gmail.com>
2018-03-20 04:34:12 +0800

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tornado-toolset',
-    version='0.0.3',
+    version='0.1.0',
     author='Sam Wu',
     author_email='samsam2310@gmail.com',
     packages=find_packages(),
